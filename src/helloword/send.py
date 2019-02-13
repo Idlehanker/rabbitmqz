aafb25ab4e840a396b4c73f1b2ca1f18c6043caf
idle <idlehanker@gmail.com>
2019-02-14 00:05:22 +0800

    init

---
+++ src/helloword/send.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+message = 'Hello World!'
+channel.queue_declare(queue='hello')
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body=message)
+
+print(" [x] Sent %r" % message)
+connection.close()
