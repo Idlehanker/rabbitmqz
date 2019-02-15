1d2aa122e72074282b8576eee5700a52f4fe8e3d
huzj2 <huzj2@cmschina.com.cn>
2019-02-15 18:52:05 +0800

    upd

---
+++ src/in-action/hello_world/consumer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import pika,sys
+
+exchange_key = 'hello-exc'
+
+credentials = pika.PlainCredentials('guest', 'guest')
+conn_params = pika.ConnectionParameters('localhost',
+                                        credentials = credentials)
+conn_broker = pika.BlockingConnection(conn_params)
+channel = conn_broker.channel()
+channel.exchange_declare(exchange=exchange_key,
+                         exchange_type='direct',
+                         passive=False,
+                         durable=True,
+                         auto_delete=False)
+
+#####
+channel.queue_declare(queue='hello-queue')
+channel.queue_bind(queue='hello-queue',
+                   exchange=exchange_key,
+                   routing_key='hola')
+
+def msg_consumer(channel, method, header, body):
+    channel.basic_ack(delivery_tag=method.delivery_tag)
+    if body == 'quit':
+        channel.basic_cancel(consumer_tag=exchange_key)
+        channel.stop_consuming()
+    else:
+        print body
+
+    return
+
+channel.basic_consume( msg_consumer,
+                       queue='hello-queue',
+                       consumer_tag='hello-consumer')
+channel.start_consuming()
